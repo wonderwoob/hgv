fe3ce0e6c19e73debad6399e08152bd43c963bad
Nathan Bills <nbills@idealshape.com>
2016-09-15 16:05:52 -0600

    Add formatting spaces

--- provisioning/roles/wordpress/templates/wp/local-config.php
+++ provisioning/roles/wordpress/templates/wp/local-config.php
@@ -1,23 +1,22 @@
 <?php
 // ** MySQL settings ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpe_{{ enviro }}' );
+define( 'DB_NAME', 'wpe_{{ enviro }}' );
 
 /** MySQL database username */
-define('DB_USER', '{{ enviro }}' );
-define('DB_USER', '{{ enviro }}');
+define( 'DB_USER', '{{ enviro }}' );
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpress' );
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '' );
+define( 'DB_COLLATE', '' );
 
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
